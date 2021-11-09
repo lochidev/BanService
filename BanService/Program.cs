1b3e67ba97da9425148f84fc8d28852ba20239c2
lochi <72388533+lochidev@users.noreply.github.com>
2021-11-09 08:22:34 +0530

    dotnet 6 upgrade

--- BanService/Program.cs
+++ BanService/Program.cs
@@ -1,21 +1,23 @@
-using Microsoft.AspNetCore.Hosting;
-using Microsoft.Extensions.Hosting;
+var builder = WebApplication.CreateBuilder(args);
 
-namespace BanService
+// Additional configuration is required to successfully run gRPC on macOS.
+// For instructions on how to configure Kestrel and gRPC clients on macOS, visit https://go.microsoft.com/fwlink/?linkid=2099682
+
+// Add services to the container.
+builder.Services.AddGrpc();
+builder.Services.AddStackExchangeRedisCache(options =>
 {
-    public class Program
-    {
-        public static void Main(string[] args)
-        {
-            CreateHostBuilder(args).Build().Run();
-        }
+    options.Configuration =
+        builder.Configuration["Redis"] ?? "localhost:6379"; //localhost:6379,password=redispwexample
+    options.InstanceName = "BanService_";
+});
 
-        // Additional configuration is required to successfully run gRPC on macOS.
-        // For instructions on how to configure Kestrel and gRPC clients on macOS, visit https://go.microsoft.com/fwlink/?linkid=2099682
-        public static IHostBuilder CreateHostBuilder(string[] args)
-        {
-            return Host.CreateDefaultBuilder(args)
-                .ConfigureWebHostDefaults(webBuilder => { webBuilder.UseStartup<Startup>(); });
-        }
-    }
-}
+var app = builder.Build();
+
+// Configure the HTTP request pipeline.
+app.MapGrpcService<BanService.Services.BannerService>();
+app.MapGet("/",
+    () =>
+        "Communication with gRPC endpoints must be made through a gRPC client. To learn how to create a client, visit: https://go.microsoft.com/fwlink/?linkid=2086909");
+
+app.Run();
